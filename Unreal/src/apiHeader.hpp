4c1df05e7876ac4d4c871048da7eb9f8ac64ff56
Sveinung Haugane <sveihaug@gmail.com>
2018-09-18 13:12:15 +0200

    Added a common headerfil for custom api-usage

---
+++ Unreal/src/apiHeader.hpp
@@ -0,0 +1,10 @@
+#include "common/common_utils/StrictMode.hpp"
+STRICT_MODE_OFF
+#ifndef RPCLIB_MSGPACK
+#define RPCLIB_MSGPACK clmdep_msgpack
+#endif // !RPCLIB_MSGPACK
+#include "rpc/rpc_error.h"
+STRICT_MODE_ON
+
+#include "vehicles/multirotor/api/MultirotorRpcLibClient.hpp"
+#include "common/common_utils/FileSystem.hpp"
