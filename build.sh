3ff9b02c385f56c7f5064123ec788a23876e310f
Sveinung Haugane <sveihaug@gmail.com>
2019-01-15 22:40:37 +0100

    Works if compiled with g++/gcc

--- build.sh
+++ build.sh
@@ -12,51 +12,21 @@ pushd "$SCRIPT_DIR" >/dev/null
 #Exit immediately on non-zero status
 set -e
 
-#**************************************
-#    AirSim prerequisites
-#**************************************
-
-#sync with AirSim repo.
-if [[ ! -d "../AirSim" ]]; then
-    echo "Build AirSim first in parent folder"
-    exit 1
-fi
-
-# check for libc++
-if [[ ! -d "./AirLib/deps/eigen3" ]]; then
-    echo "ERROR: Eigen 3 not found. Please rebuild AirSim"
-    exit 1
-fi
-
-# check for rpclib
-if [ ! -d "./AirLib/deps/rpclib" ]; then
-    echo "ERROR: rpclib not found. Please rebuild AirSim"
-    exit 1
-fi
-
-# check for cmake build
-if [ ! -d "./AirLib/deps/MavLinkCom" ]; then
-    echo "ERROR: MavLinkCom dependency not found. Rebuild AirSim"
-    exit 1
-fi
-
-rsync -a --delete ../AirSim/AirLib/ AirLib
-rsync -a --delete ../AirSim/external/ external
-
 echo ""
 echo "************************************"
 echo "Building ROS packages with catkin"
 echo "************************************"
 
-export CC=/usr/bin/clang
-export CXX=/usr/bin/clang++
+if [ "$1" == "gcc" ]; then
+	export CC="gcc"
+    export CXX="g++"
+else
+	export CC="clang-5.0"
+	export CXX="clang++-5.0"
+fi
 
 pushd catkin_ws >/dev/null
-catkin_make	
+catkin_make	--cmake-args -DCMAKE_BUILD_TYPE="debug" -Wall -Wextra
 popd >/dev/null
 
 popd >/dev/null
-
-
-
-
--- catkin_ws/src/ros_api/src/ros_api.cpp
+++ catkin_ws/src/ros_api/src/ros_api.cpp
@@ -6,43 +6,42 @@ STRICT_MODE_OFF
 #ifndef RPCLIB_MSGPACK
 #define RPCLIB_MSGPACK clmdep_msgpack
 #endif // !RPCLIB_MSGPACK
-//#include "rpc/rpc_error.h"
+#include "rpc/rpc_error.h"
 STRICT_MODE_ON
 
 #include <iostream>
-#include <string>
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
 #include "vehicles/multirotor/api/MultirotorRpcLibClient.hpp"
 
-namespace msr {namespace airlib {
-
-void connect(MultirotorRpcLibClient& client) {
+void connect(msr::airlib::MultirotorRpcLibClient& client) {
 	try {
 		client.confirmConnection();
 
 		ROS_INFO("Connedted to multirotor simulation");
 	}
-	catch(...) {
-		std::cout << "Error!" << std::endl;
-	}
 	
-/*	catch (rpc::rpc_error&  e) {
+	catch (rpc::rpc_error&  e) {
         std::string msg = e.get_error().as<std::string>();
         std::cout << "Exception raised by the API, something went wrong." << std::endl << msg << std::endl;
-	}*/
+	}
 }
 
 void screenLoggerCallback(const std_msgs::String::ConstPtr& msg) {
 	ROS_INFO("Got: [%s]", msg->data.c_str());
+	std::cout << "Woah! Melding!" << std::endl;
 }
 
-int main(int argc, char** argv) {
 
-	MultirotorRpcLibClient client;
+
+int main(int argc, char* argv[]) {
+
+	msr::airlib::MultirotorRpcLibClient client;
 	connect(client);
+
+	ROS_INFO("Starting Multirotor node!");
 	
 	ros::init(argc, argv, "multirotor");
 	ros::NodeHandle n;
@@ -50,9 +49,8 @@ int main(int argc, char** argv) {
 	ros::Subscriber sub = n.subscribe("test_msg", 1000, screenLoggerCallback);
 
 	ros::spin();
-
+	std::cout << "hei" << std::endl;
 	return 0;
 }
 
-}} //namespace msr::airlib
 #endif // ros_api
